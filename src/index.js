8a439cf5457a462c9194b7ac0946a2b552387787
SobolNikita <soboln2006@gmail.com>
2024-08-12 14:48:08 +0300

    fix: module.exports in index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-function towelSort (matrix) {
-  let ans = [];
-  for(let i = 0; i < matrix.length; ++i){
-    if(i%2===0){
-      matrix[i].forEach(e=>{
-        ans.push(e)
-      });
-    }else{
-      for(let j = matrix[i].length-1; j >= 0; --j){
-        ans.push(matrix[i][j]);
-      }
-    }
+module.exports = function towelSort(matrix) {
+  if(matrix===undefined){
+    return [];
   }
-  return ans;
-}
+    let ans = [];
+    for (let i = 0; i < matrix.length; ++i) {
+        if (i % 2 === 0) {
+            matrix[i].forEach((e) => {
+                ans.push(e);
+            });
+        } else {
+            for (let j = matrix[i].length - 1; j >= 0; --j) {
+                ans.push(matrix[i][j]);
+            }
+        }
+    }
+    return ans;
+};
